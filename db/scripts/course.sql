4e5160e03df6738efc5c17720f2b17fefd509669
janani.mohan <janani.mohan@aspiersys.com>
2020-11-17 17:36:14 +0000

    initial commit

---
+++ db/scripts/course.sql
@@ -0,0 +1,2 @@
+create schema schema2;
+create table schema2.course(name varchar2(100), age number)
---
+++ db/scripts/health.sql
@@ -0,0 +1,2 @@
+create schema schema1;
+create table schema1.health(name varchar2(100), age number)
